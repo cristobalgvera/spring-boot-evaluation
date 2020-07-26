75cf49313dd6e95e1c0eb078d7c431c6452c54ac
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-07-26 01:03:05 -0400

    repo init

---
+++ src/main/java/cl/fullstack/nomasaccidentabilidad/NoMasAccidentabilidadApplication.java
@@ -0,0 +1,13 @@
+package cl.fullstack.nomasaccidentabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoMasAccidentabilidadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoMasAccidentabilidadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/fullstack/nomasaccidentabilidad/NoMasAccidentabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.fullstack.nomasaccidentabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoMasAccidentabilidadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
